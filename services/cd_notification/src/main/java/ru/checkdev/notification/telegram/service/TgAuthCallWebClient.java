f1fdc84a27e758b7465be0b78595df8e1a11383a
v.krasov.ext <v.krasov@aleria.cloud>
2026-03-10 00:06:57 +0400

    use resilience4j

--- services/cd_notification/src/main/java/ru/checkdev/notification/telegram/service/TgAuthCallWebClient.java
+++ services/cd_notification/src/main/java/ru/checkdev/notification/telegram/service/TgAuthCallWebClient.java
@@ -1,15 +1,13 @@
 package ru.checkdev.notification.telegram.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import ru.checkdev.notification.circuitbreaker.CircuitBreaker;
 import ru.checkdev.notification.domain.Profile;
-import ru.checkdev.notification.retry.Retry;
 import ru.checkdev.notification.service.EurekaUriProvider;
 
 /**
@@ -33,20 +31,15 @@ public class TgAuthCallWebClient implements TgCall {
      * @return Mono<Person>
      */
     @Override
+    @Retry(name = "authService")
+    @CircuitBreaker(name = "authService", fallbackMethod = "fallbackGet")
     public Mono<Profile> doGet(String url) {
-        var retry = new Retry(3, 1000);
-        var cb = new CircuitBreaker(2);
-        Mono<Profile> defVal = Mono.error(new RuntimeException("All retry attempts failed"));
-        return retry.exec(() ->
-            cb.exec(() -> WebClient.create(uriProvider.getUri(SERVICE_ID))
-                .get()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(Profile.class)
-                .doOnError(err -> log.error("API not found: {}", err.getMessage())),
-                defVal),
-            defVal
-            );
+        return WebClient.create(uriProvider.getUri(SERVICE_ID))
+            .get()
+            .uri(url)
+            .retrieve()
+            .bodyToMono(Profile.class)
+            .doOnError(err -> log.error("API not found: {}", err.getMessage()));
     }
 
     /**
@@ -57,37 +50,27 @@ public class TgAuthCallWebClient implements TgCall {
      * @return Mono<Person>
      */
     @Override
+    @Retry(name = "authService")
+    @CircuitBreaker(name = "authService", fallbackMethod = "fallbackGet")
     public Mono<Object> doPost(String url, Profile profile) {
-        var retry = new Retry(3, 1000);
-        var cb = new CircuitBreaker(2);
-        Mono<Object> defVal = Mono.error(new RuntimeException("All retry attempts failed"));
-        return retry.exec(() ->
-            cb.exec(() -> WebClient.create(uriProvider.getUri(SERVICE_ID))
-                .post()
-                .uri(url)
-                .bodyValue(profile)
-                .retrieve()
-                .bodyToMono(Object.class)
-                .doOnError(err -> log.error("API not found: {}", err.getMessage())),
-                defVal),
-            defVal
-        );
+        return WebClient.create(uriProvider.getUri(SERVICE_ID))
+            .post()
+            .uri(url)
+            .bodyValue(profile)
+            .retrieve()
+            .bodyToMono(Object.class)
+            .doOnError(err -> log.error("API not found: {}", err.getMessage()));
     }
 
     @Override
+    @Retry(name = "authService")
+    @CircuitBreaker(name = "authService", fallbackMethod = "fallbackGet")
     public Mono<Object> doPost(String url) {
-        var retry = new Retry(3, 1000);
-        var cb = new CircuitBreaker(2);
-        Mono<Object> defVal = Mono.error(new RuntimeException("All retry attempts failed"));
-        return retry.exec(() ->
-            cb.exec(() -> WebClient.create(uriProvider.getUri(SERVICE_ID))
-                .post()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(Object.class)
-                .doOnError(err -> log.error("API not found: {}", err.getMessage())),
-                defVal),
-            defVal
-        );
+        return WebClient.create(uriProvider.getUri(SERVICE_ID))
+            .post()
+            .uri(url)
+            .retrieve()
+            .bodyToMono(Object.class)
+            .doOnError(err -> log.error("API not found: {}", err.getMessage()));
     }
 }
