9f4af231dd2b9a69b6017f39d97a66b1ef194d61
v.krasov.ext <v.krasov@aleria.cloud>
2026-03-13 23:40:34 +0400

    [generator] add Dockerfile and actuator

--- services/cd_generator/src/main/java/ru/checkdev/generator/config/SecurityConfig.java
+++ services/cd_generator/src/main/java/ru/checkdev/generator/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(
                 HttpMethod.GET,
+                "/actuator/health",
                 "/exam/**",
                 "/statistic/**")
                 .and()
